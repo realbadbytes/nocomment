8b98e1adca80afe60cbe587f976ca7d87bd8cf0b
realbadbytes <badbytes.io@gmail.com>
2018-10-26 15:43:23 -0700

    starting to create project structure

---
+++ nocomment.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Main nocomment script."""
+
+
+import logging
+import time
+
+
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
+
+
+def main():
+    """ Main nocomment script."""
+    logging.info('nocomment starts')
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_targets/target1.py
@@ -0,0 +1,4 @@
+""" Fake target 1. """
+
+def function1(arg1, arg2, arg3):
+    return 42
