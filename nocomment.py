c2a809b94ff54ebb03ad25b90ed4576dd99bb90c
realbadbytes <badbytes.io@gmail.com>
2018-10-26 17:43:27 -0700

    survey target modules for undocumented functions

--- nocomment.py
+++ nocomment.py
@@ -2,6 +2,10 @@
 """ Main nocomment script."""
 
 
+#== TESTING ======================= 
+from test_targets import target1
+#== TESTING =======================
+
 import logging
 import time
 
@@ -9,9 +13,43 @@ import time
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
 
 
+def get_docstrings(target):
+    """ Parses docstrings in target module.
+
+    :param target: loaded python module
+    :returns: dict of functions and their docstring
+    """
+    docstrings = {}
+    for funcname, funcptr in target.__dict__.items():
+        if callable(funcptr):
+            logging.info('Found callable: {}'.format(funcname))
+            docstrings[funcname] = funcptr.__doc__
+    return docstrings
+
+
+def check_docstring_exist(func_docstring_dict):
+    """ Checks processed functions for docstrings.
+
+    :param func_docstring_dict: dict of functions and their associated docstrings
+    :returns: list of undocumented functions
+    """
+    empty_docstrings = []
+    for funcname, docstring in func_docstring_dict.items():
+        if docstring is None:
+            empty_docstrings.append(funcname)
+    return empty_docstrings
+
+
 def main():
     """ Main nocomment script."""
     logging.info('nocomment starts')
+    # Check all functions in target file for valid docstring
+    docstrings = get_docstrings(target1)
+    logging.info('Processed {0} functions in target module.'.format(len(docstrings)))
+    # Invalid or non-existent docstring? Prompt for comments
+    undoc_funcs = check_docstring_exist(docstrings)
+    logging.info('Function(s) without proper documentation: {0}'.format(' '.join(undoc_funcs)))
+    # Build docstring
 
 
 if __name__ == '__main__':
--- test_targets/target1.py
+++ test_targets/target1.py
@@ -1,4 +1,16 @@
-""" Fake target 1. """
+""" Fake target file 1."""
 
-def function1(arg1, arg2, arg3):
+
+def function_42(a):
+    """
+    A short function_42 description.
+
+    :param a: a placeholder first param
+    :type a: int
+    :returns: int 42
+    """
     return 42
+
+
+def function_1337(a, b, c):
+    return 1337
